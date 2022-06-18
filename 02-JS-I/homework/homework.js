a8dbb8336be3964e81bd7728646b010a1a2f6ca3
FFigueroa26 <figueroafelix89@gmail.com>
2022-06-18 00:36:14 -0500

    Java 1 Listo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -132,8 +132,9 @@ function esImpar(num) {
   // Tu código:
   if (num % 2 == 0) {
     return false;
-  }
+  } else {
   return true;
+  }
 }
 
 function elevarAlCuadrado(num) {
@@ -173,17 +174,24 @@ function numeroRandom() {
   return Math.random();
 }
 
+
 function esPositivo(numero) {
   //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
- if (numero > 0) {
+if (numero === 0) {
+  return false;
+} 
+else if (numero > 0) {
   return "Es positivo";
- }
- else "Es negativo";
-  }
-  
+}
+else {
+  return "Es negativo";
+}
+}
+
+
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
