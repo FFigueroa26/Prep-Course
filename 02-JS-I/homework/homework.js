45b3841dc14b0fcc814eb5736ea51b706147623a
FFigueroa26 <figueroafelix89@gmail.com>
2022-06-18 22:32:00 -0500

    Tarde de Java

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -130,7 +130,7 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num % 2 == 0) {
+  if (num % 2 === 0) {
     return false;
   } else {
   return true;
